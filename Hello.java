d5a94598692e116cdb1bc6cf5ba40ebed249501f
PranaliBavdhankar <103841885+PranaliBavdhankar@users.noreply.github.com>
2023-06-08 11:37:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,26 @@
-class Hello{
-  public static void main(String args[]){
-    System.out.println("Hello, I am Pranali here!");
-  }
-}
+public class Hello{
+
+    public static void main(String[] args) {
+
+        
+            int limit = 100;
+
+            System.out.println("Prime numbers between 1 and " + limit);
+
+            for(int i=1; i < 100; i++){
+
+                    boolean isPrime = true;
+
+                    //check to see if the number is prime
+                    for(int j=2; j < i ; j++){
+
+                            if(i % j == 0){
+                                    isPrime = false;
+                                    break;
+                            }
+                    }
+                    
+                    if(isPrime)
+                            System.out.print(i + " ");
+            }
+    }
